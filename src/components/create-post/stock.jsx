aaef1644a064da38bf8592509b695494b4639c62
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-06 13:38:36 +0500

    updated

--- src/components/create-post/stock.jsx
+++ src/components/create-post/stock.jsx
@@ -1,13 +1,15 @@
 import useStockSearch from "../../hooks/useStockSearch";
 import ModalHeader from "../../ui/modal/header";
+import Search from "../../ui/search";
+import SearchWrapper from "../search/wrapper";
 
 
 function Main(props) {
     const { results, onAdd, ...sProps } = useStockSearch({ value: props.stock_symbol, api: "fetch_stock" });
     return (
-        <>
-
-        </>
+        <div>
+            <Search placeholder="Search" {...sProps} />
+        </div>
     )
 }
 
--- src/ui/search/index.jsx
+++ src/ui/search/index.jsx
@@ -1,13 +1,12 @@
 import Magnifier from '../../icons/magnifier'
+import Pluse from '../../icons/plus'
 import styles from './styles.module.scss'
 
-export default function Search({ className, children, ...props }) {
+export default function Search({ className, children, onClear, ...props }) {
     return (
         <label className={`${styles.root} flex-center ${className}`}>
             <input type="text" {...props} />
-            <Magnifier className="text-light-3" />
+            {props.value && onClear ? <button onClick={onClear} type='button' className='btn-icon no-space'><Pluse className="rotate-45" /></button> : <Magnifier className="text-light-3" />}
         </label>
     )
 }
-
-// cont-p gap-8 backdrop-blur-lg bg-white bg-opacity-80 nav-height border-light-0 border-b flex-center
