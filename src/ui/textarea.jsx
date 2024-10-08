33c25a8803794b67b66ce8a405c91e3a1a5a1482
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-08 09:35:57 +0500

    updated

--- src/ui/textarea.jsx
+++ src/ui/textarea.jsx
@@ -1,5 +1,16 @@
 import withField from "../hoc/withField"
 
-const TextArea = props => <textarea {...props} />
+const TextArea = props => {
+    const handleInput = event => {
+        event.target.style.height = 'auto';
+        event.target.style.height = `${event.target.scrollHeight}px`;
+    };
+
+    const handleOnInput = event => {
+        handleInput(event);
+    };
+
+    return <textarea {...props} onInput={handleOnInput} />;
+}
 
 export default withField(TextArea);
