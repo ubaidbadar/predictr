d44f34a52b8bc5a9dc3bba765810b698af0057f3
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-09 21:53:38 +0500

    updated

--- src/components/post/Movement/Movement.jsx
+++ src/components/post/Movement/Movement.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import CheckCircle from "../../../icons-v2/CheckCircle";
-import CrossCircle from "../../../icons-v2/CrossCircle";
+// import CheckCircle from "../../../icons-v2/CheckCircle";
+// import CrossCircle from "../../../icons-v2/CrossCircle";
 import styles from './Movement.module.scss';
-import moment from "moment/moment";
-import Clock from "../../../icons-v2/Clock";
-import MinusCircle from "../../../icons-v2/MinusCircle";
+import moment from "moment-timezone";
+// import Clock from "../../../icons-v2/Clock";
+// import MinusCircle from "../../../icons-v2/MinusCircle";
 
 
 const Movement = ({ percentage = '0', accurate = true, guess_date, actual_change,points }) => {
     const guessDate = moment(guess_date);
     const days_left = guessDate.diff(moment(), 'days');
     const is_day_left = days_left > 0;
-    const leftHTML = is_day_left && <span className='me-auto text-normal d-flex gap-1 align-items-center'><Clock />{days_left} day{days_left > 1 && 's'} left</span>
-    const isCompetition = window.location.href.includes('/competition')
+    const leftHTML = is_day_left && <span className='me-auto text-normal d-flex gap-1 align-items-center'>
+        {/* <Clock /> */}
+        {days_left} day{days_left > 1 && 's'} left</span>
+    // const isCompetition = window.location.href.includes('/competition')
     if(actual_change === 0) return (
         <div className={`${styles.root} text-normal ${styles.daysLeft}`}>
-            <MinusCircle />
+            {/* <MinusCircle /> */}
             <b>No movement</b>
             {leftHTML}
         </div>
@@ -23,7 +25,7 @@ const Movement = ({ percentage = '0', accurate = true, guess_date, actual_change
     
     return (
         <div className={`${styles.root} ${is_day_left ? styles.daysLeft : ''} text-accent-${accurate ? 3 : 2}`}>
-            {isCompetition ? <span className={styles.points} aria-details={points.toFixed(2)} /> : (accurate ? <CheckCircle /> : <CrossCircle />)}
+            {/* {isCompetition ? <span className={styles.points} aria-details={points.toFixed(2)} /> : (accurate ? <CheckCircle /> : <CrossCircle />)} */}
             <span className="fw-semibold">{accurate ? <>{percentage}% Accura</> : 'Inaccura'}{is_day_left ? 'cy' : 'te'}</span>
             {leftHTML}
         </div>
