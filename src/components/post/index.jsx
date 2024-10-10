c32968f4d1e616573c6a159b28c46a10c7ef7b32
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-10 14:35:46 +0500

    updated

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -3,9 +3,9 @@ import User from "../user";
 import dayjs from "dayjs";
 import plugin from "dayjs/plugin/relativeTime";
 import Buttons from "./buttons";
-import { Tooltip } from "@mui/material";
 dayjs.extend(plugin)
 import styles from './styles.module.scss'
+import ToolTip from "../../ui/tooltip";
 
 export default function Post(props) {
     const postedOn = dayjs(props.createdOn), isCurrent = props.userId._id === props.user._id, guess_date = dayjs(props.guess_date)
@@ -20,16 +20,16 @@ export default function Post(props) {
                     />
                     {!isCurrent && <Buttons {...props} />}
                 </div>
-                <div className={`d-flex subtitle text-normal fs-normal-sm flex-wrap align-items-center gap-2 ${styles.main}`}>
-                    I think <Tooltip title={props.stock_name}><b>{props.stock_symbol}</b></Tooltip> will go {props.allowed ? (
+                <div className={`flex-center flex-wrap gap-2 ${styles.main}`}>
+                    I think <ToolTip title={props.stock_name}><b>{props.stock_symbol}</b></ToolTip> will go {props.allowed ? (
                         <>
-                            <b>{props.estimated_direction} {props.estimated_change_percent}%</b> by <Tooltip title={guess_date.format('LL')}><b>{guess_date.format('MMM DD')}</b></Tooltip>
+                            <b>{props.estimated_direction} {props.estimated_change_percent}%</b> by <ToolTip title={guess_date.format('LL')}><b>{guess_date.format('MMM DD')}</b></ToolTip>
                             {props.description && <p className={`my-1 w-100`}>{props.description}</p>}
                         </>
                     ) : (
                         <>
                             <b className={styles.blur}>del%</b> by <b className={styles.blur}>del Date</b>
-                            {props.description && <p className={`my-0 w-100 p-1 ${styles.blur}`}>{props.description}</p>}
+                            {props.description && <p className={`my-0 w-100 p-1 ${styles.blur}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut pariatur explicabo laboriosam repellendus molestias accusantium. Doloremque expedita quas magni. Eos delectus recusandae odit corporis, totam suscipit omnis ipsam fugit hic?</p>}
                         </>
                     )}
                 </div>
---
+++ src/ui/tooltip/index.jsx
@@ -0,0 +1,5 @@
+import Tooltip from "@mui/material/Tooltip";
+
+export default function ToolTip(props) {
+    return <Tooltip {...props} arrow placement="top" />
+}
