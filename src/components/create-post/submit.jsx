b15affccf29a78ad992eb26bbaa59ef57be41558
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-10 15:26:23 +0500

    updated

--- src/components/create-post/submit.jsx
+++ src/components/create-post/submit.jsx
@@ -5,11 +5,9 @@ function Main(props) {
     const { loading, err, res, submit } = usePost();
     return <button className={`btn-primary${loading ? " loading" : ""}`}
         onClick={() => {
-            const data = new FormData();
-            if(typeof props.form.guess_date !== 'string') {
-                props.form.guess_date = props.form.guess_date.toISOString();
-            }
-            for (let key in props.form) props.form[key] !== undefined && data.append(key, props.form[key]);
+            const data = new FormData(), form = {...props.form}
+            form.guess_date = props.form.guess_date.toISOString();
+            for (const key in form) props.form[key] !== undefined && data.append(key, props.form[key]);
             submit({
                 method: 'POST',
                 url: '/leaderboard_post',
