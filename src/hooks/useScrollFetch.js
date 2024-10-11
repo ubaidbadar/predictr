20a94aead592cae835fc9e46544b7ba4d134d78f
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-11 15:57:22 +0500

    fixed-stuff

--- src/hooks/useScrollFetch.js
+++ src/hooks/useScrollFetch.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react"
 import getAxiosMessage from "../lib/getAxiosMessage";
 
 
-let hasEvent = false;
 
 const useScrollFetch = (api, posts = []) => {
     const [state, setState] = useState({
@@ -15,33 +14,31 @@ const useScrollFetch = (api, posts = []) => {
         const p = [...state.posts];
         let exists = true, other = {};
         const cb = async (e) => {
-            if (!exists || (typeof e === 'object' && (window.innerHeight + window.scrollY + 200)) >= document.body.offsetHeight) return;
-            exists = false;
-            setState({ posts, exists: true });
-            const search = new URLSearchParams(location.search);
-            search.set('page', (p.length / 20) + 1);
-            try {
-                const { posts, ...data } = (await axios.get(`${api}?${search.toString()}`)).data
-                other = { ...other, ...data };
-                p.push(...posts);
-                exists = posts.length === 20;
-                setState({ posts: p, exists, other });
-            }
-            catch (err) {
-                window.removeEventListener('scroll', cb);
-                setState({ posts: p, exists: false })
-                getAxiosMessage(err);
+            if (exists && (window.innerHeight + window.scrollY + 200) >= document.body.offsetHeight) {
+                exists = false;
+                setState({ posts: p, exists: true });
+                const search = new URLSearchParams(location.search);
+                search.set('page', (p.length / 20) + 1);
+                try {
+                    const { posts, ...data } = (await axios.get(`${api}?${search.toString()}`)).data
+                    other = { ...other, ...data };
+                    p.push(...posts);
+                    console.log(p)
+                    exists = posts.length === 20;
+                    setState({ posts: p, exists, other });
+                }
+                catch (err) {
+                    window.removeEventListener('scroll', cb);
+                    setState({ posts: p, exists: false })
+                    getAxiosMessage(err);
+                }
             }
 
         }
-        if(!hasEvent) {
-            console.log(hasEvent)
-            hasEvent = true;
-            window.addEventListener('scroll', cb);
-            cb();
-            return () => {
-                window.removeEventListener('scroll', cb);
-            }
+        window.addEventListener('scroll', cb);
+        cb();
+        return () => {
+            window.removeEventListener('scroll', cb);
         }
     }, [location.search])
     return state;
