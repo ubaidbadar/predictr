b0f1fc5f248086b93691526221fad6438eaf7055
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-12 20:31:35 +0500

    fixed-stuff

--- src/components/header/search.jsx
+++ src/components/header/search.jsx
@@ -1,12 +1,19 @@
 import Magnifier from '../../icons/magnifier';
+import Pluse from '../../icons/plus';
 import Search from '../../ui/search';
 import styles from './search.module.scss';
 
 const S = (props) => {
     return (
         <>
-            <Search id="Nav-Search" className={styles.root} placeholder="Search Predictr..." />
-            {props.isLoggedIn && (
+            <Search id="Nav-Search" className={styles.root} placeholder="Search Predictr...">
+                {props.isMobile && (
+                    <span className='btn-icon absolute right-0'>
+                        <Pluse className="rotate-45" />
+                    </span>
+                )}
+            </Search>
+            {props.isLoggedIn && props.isMobile && (
                 <label htmlFor='Nav-Search' className='btn-icon text-gray-0 md:hidden no-space'>
                     <Magnifier />
                 </label>
--- src/ui/search/index.jsx
+++ src/ui/search/index.jsx
@@ -6,7 +6,8 @@ export default function Search({ className, children, onClear, ...props }) {
     return (
         <label className={`${styles.root} flex-center ${className}`}>
             <input type="text" {...props} />
-            {props.value && onClear ? <button onClick={onClear} type='button' className='btn-icon no-space'><Pluse className="rotate-45" /></button> : <Magnifier className="text-light-3" />}
+            {props.value && onClear ? <button onClick={onClear} type='button' className='btn-icon no-space'><Pluse className="rotate-45" /></button> : !children && <Magnifier className="text-light-3" />}
+            {children}
         </label>
     )
 }
