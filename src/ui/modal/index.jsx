80bb7f581f7fac2f3cee0b957f2b5a3f5013b8fc
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-12 20:21:30 +0500

    fixed-stuff

--- src/ui/modal/index.jsx
+++ src/ui/modal/index.jsx
@@ -1,8 +1,8 @@
 import ModalBase from "../modal_base";
 import ModalHeader from "./header";
 
-export default function Modal({ title, id, children, ...props }) {
-    const mid = id || title;
+export default function Modal({ id, children, ...props }) {
+    const mid = id || props.title;
     return (
         <ModalBase {...props} id={mid} className="Modal-0">
             <ModalHeader {...props} id={mid}  />
