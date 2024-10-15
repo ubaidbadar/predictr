4a513fd2a8af9eb6e8e159b2e630bf06c0cd2850
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-15 16:24:13 +0500

    updated

--- src/components/post-old/PostComments/index.jsx
+++ src/components/post-old/PostComments/index.jsx
@@ -2,26 +2,21 @@ import React, { useState } from 'react';
 import AddComment from '../AddComment';
 import Comments from '../Comments';
 import LoadMore from '../LoadMore';
-// import useSocket from '../../../hooks/useSocket';
 
 const PostComments = ({ comments = [], total_comments = 0, total_replies = 0, ...props }) => {
     const [status, setStatus] = useState({
         cms: [...comments],
         total: total_comments + total_replies,
         total_cms: total_comments,
-    });
-    const { cms, total, total_cms } = status;
-    const onAdd = comment => {
-        setStatus({ cms: [comment, ...cms], total: total + 1, total_cms: total_cms + 1 });
-    }
+    }), { cms, total, total_cms } = status;
+    const onAdd = comment => setStatus({ cms: [comment, ...cms], total: total + 1, total_cms: total_cms + 1 });
     const onLoad = comments => setStatus({ ...status, cms: cms.length === 1 ? comments : [...cms, ...comments] });
     const onReply = () => setStatus({ ...status, total: total + 1 });
-    console.log(props)
     return (
         <>
-            <AddComment {...props} _id={props.postId} onAdd={onAdd} className='my-3' />
+            {props.isLoggedIn && <AddComment {...props} _id={props.postId} onAdd={onAdd} className='my-3' />}
             {total > 0 && (
-                <div className='card-v3 gap-4'>
+                <div className='card-v3 mt-3 gap-4'>
                     <Comments {...props} comments={cms} onReply={onReply} />
                     {total_cms > cms.length && (
                         <LoadMore {...props} onLoad={onLoad}
