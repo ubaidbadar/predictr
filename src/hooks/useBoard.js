7ad7139809b417512c43436b7d7d5f3cf9d6d2c5
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-15 16:19:39 +0500

    Refactor useBoard hook to remove unused code and improve readability

--- src/hooks/useBoard.js
+++ src/hooks/useBoard.js
@@ -1,8 +1,7 @@
-import { useNavigate } from "react-router-dom";
 import useAuth from "../contexts/auth";
 
 
 export default function useBoard() {
-    const navigate = useNavigate(), auth = useAuth(), gaurd = () => !auth.isLoggedIn && (location.hash = "#login");    
-    return { ...auth, gaurd, navigate }
+    const auth = useAuth(), gaurd = () => !auth.isLoggedIn && (location.hash = "#login");    
+    return { ...auth, gaurd }
 }
