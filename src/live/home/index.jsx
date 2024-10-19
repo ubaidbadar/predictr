8f70bf244f326e9259a12d532b7cd933508cc175
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-19 22:49:18 +0500

    Refactor Leaderboard component to conditionally render Left component based on isMobile value

--- src/live/home/index.jsx
+++ src/live/home/index.jsx
@@ -1,15 +1,16 @@
 import PrizeBanner from "../../components/prize-banner";
 import useBoard from "../../hooks/useBoard";
+import useQuery from "../../hooks/useQuery";
 import Left from "./left";
 import Main from "./main";
 
 export default function Leaderboard() {
-    const props = useBoard();
+    const props = useBoard(), isMobile = useQuery();
     return (
         <>
             <PrizeBanner {...props} />
             <div className="cont leaderboard">
-                <Left {...props} />
+                {!isMobile && <Left {...props} />}
                 <Main {...props} />
             </div>
         </>
