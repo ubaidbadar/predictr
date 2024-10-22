7efaf44791939d1631ad731c86fdc331cd6278e6
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-22 21:34:38 +0500

    updated

--- src/components/top-leaderboard/index.jsx
+++ src/components/top-leaderboard/index.jsx
@@ -6,9 +6,10 @@ export default function TopLeaderBoard(props) {
         <div className={`card mt-4 sticky top-nav ${props.className}`}>
             <div className="flex-between pb-4 border-b border-light-4">
                 <h3>Leaderboard</h3>
-                <Select component={props => <button {...props} className="btn-text btn-select">{props.value} <Down /></button>}>
+                <Select value="All" component={props => <button {...props} className="btn-text btn-select">{props.value}</button>}>
                     <Item value="All">All</Item>
                     <Item value="Month">Month</Item>
+                    <Item value="Week">Week</Item>
                 </Select>
             </div>
         </div>
