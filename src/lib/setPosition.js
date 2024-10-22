dac5defa6aa348e87403ab611e8a812876e1fb1d
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-10-22 20:20:49 +0500

    Refactor setPosition to improve element positioning

--- src/lib/setPosition.js
+++ src/lib/setPosition.js
@@ -4,12 +4,12 @@ const setPosition = (tag1, tag2) => {
     const cb = () => {
         const doc = document.documentElement, domWidth = window.innerWidth || doc.clientWidth, domHeight = window.innerHeight || doc.clientHeight;
         const style = tag2.style, rect1 = tag1.getBoundingClientRect();
-        style.left = rect1.x + 'px';
-        style.top =  rect1.bottom + 'px';
+        style.right = (domWidth - rect1.right) + 'px';
+        style.top = rect1.bottom + 'px';
         const rect2 = tag2.getBoundingClientRect();
         if (rect2.left < 0 || rect2.right > domWidth) {
-            style.left = 'initial';
-            style.right = (domWidth - rect1.right) + 'px'
+            style.right = 'initial';
+            style.left = rect1.x + 'px';
         }
         if (!(rect2.top >= 0 && rect2.bottom <= domHeight)) {
             style.top = (rect1.bottom - rect2.height - rect1.height) + 'px';
