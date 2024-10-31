abc071ea9572f8ee96bbfa659f0e3b103ed6310f
Ubaid Badar <m.ubaidbadar@gmail.com>
2024-10-31 16:46:29 +0500

    Refactor PrizeBanner component to improve timer functionality and update styling for better layout

--- src/components/prize-banner/index.jsx
+++ src/components/prize-banner/index.jsx
@@ -12,18 +12,18 @@ const PrizeBanner = (props) => {
     const [active, setActive] = useState(false);
     const handleTimer = (active = false) => {
         setActive(active);
-        // clearInterval(timer);
-        // timer = setInterval(() => {
-        //     active = !active;
-        //     setActive(active);
-        // }, 5000);
+        clearInterval(timer);
+        timer = setInterval(() => {
+            active = !active;
+            setActive(active);
+        }, 5000);
     }
     useEffect(() => {
         handleTimer();
         return () => clearInterval(timer);
     }, [])
     return (
-        <div className='sm:m-2 m-1 md:mx-cont overflow-hidden relative text-surface'>
+        <div className='md:mx-cont lg:mt-4 md:mt-3 mt-2 mx-2 overflow-hidden relative text-surface'>
             <div className={`relative flex ${styles.root}`} style={{ left: active ? '-100%' : '0' }}>
                 <First />                
                 <div className={`d-flex bg-secondary-0 mr-auto flex-column align-items-md-center flex-md-row gap-3 p-4 pb-5 border-radius-8 ${styles.banner2}`}>
