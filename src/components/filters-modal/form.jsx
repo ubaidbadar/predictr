a149a367f8b45d88175ce53c2806aa34ed9ac814
Ubaid Badar <m.ubaidbadar@gmail.com>
2024-11-10 21:52:39 +0500

    Refactor FiltersModal form to initialize values from URL search parameters and update reset functionality

--- src/components/filters-modal/form.jsx
+++ src/components/filters-modal/form.jsx
@@ -6,23 +6,27 @@ import { useState } from "react";
 import updateSearchParams from "../../lib/updateSearchParams";
 
 
-const d = {
-    title: "",
-    end_date: "",
-    start_date: "",
-    accuracy: "",
-    hashtags: ""
+export default function Form() {
+    const search = new URLSearchParams(window.location.search);
+    const d = {
+        accuracy: "",
+        start_date: "",
+        end_date: "",
+        hashtags: "",
+    }
+    const values = {};
+    for (const key of Object.keys(d)) values[key] = search.get(key) || "";
+    return <Main d={d} values={values} />
 }
 
-export default function Form() {
-    const [values, setValues] = useState(d);
+function Main(props) {
+    const [values, setValues] = useState(props.values);
     const onChange = e => setValues({ ...values, [e.target.name]: e.target.value });
     return (
         <>
-            <TextField name="accuracy" onChange={onChange} placeholder="Enter manually accuracy" title="Accuracy (%)" />
+            <TextField name="accuracy" value={values.accuracy} onChange={onChange} placeholder="Enter manually accuracy" title="Accuracy (%)" />
             <RangePicker
                 onChange={e => {
-                    console.log(e)
                     setValues({ ...values, start_date: e.start, end_date: e.end })
                 }}
                 component={tools => (
@@ -49,8 +53,8 @@ export default function Form() {
             <TextField name="hashtags" onChange={onChange} value={values.hashtags} left={<Magnifier />} placeholder="#tags" title="HashTags" />
             <div className="Modal-Footer gap-5">
                 <button type="button" className="btn-text" onClick={() => {
-                    setValues(d);
-                    updateSearchParams({})
+                    setValues(props.d);
+                    updateSearchParams(props.d)
                 }}>Reset</button>
                 <button className="btn-primary">Save</button>
             </div>
