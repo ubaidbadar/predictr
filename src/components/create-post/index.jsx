8ca740fb7c06919c127367ea32bb11b8eda80ed8
Ubaid Badar <m.ubaidbadar@gmail.com>
2024-11-10 21:54:21 +0500

    Update CreatePost button styles for improved alignment and padding

--- src/components/create-post/index.jsx
+++ src/components/create-post/index.jsx
@@ -7,9 +7,9 @@ export default function CreatePost(props) {
     const tools = useShow();
     return (
         <>
-            <button onClick={tools.open} className="card-v3 gap-2 flex">
+            <button onClick={tools.open} className="card-v3 gap-2 flex-center">
                 <Avatar {...props.user} className="h3" />
-                <span className="bg-light-1 rounded-2 h-full flex-center text-gray-0 px-4 flex-1">Make a prediction, Trevor</span>
+                <span className="bg-light-1 rounded-2 flex-center text-gray-0 px-4 py-2 flex-1">Make a prediction, Trevor</span>
             </button>
             {tools.className && (
                 <Modal className={`Modal-0 ${tools.className}`}>
