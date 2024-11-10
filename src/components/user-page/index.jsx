6618fd624135cbb07a7e5c6baa67fc17d4b3ecc6
Ubaid Badar <m.ubaidbadar@gmail.com>
2024-11-10 23:05:18 +0500

    Enhance UserPage with improved error handling and loading state; update Filters component to be sticky and adjust styles for better layout

--- src/components/user-page/index.jsx
+++ src/components/user-page/index.jsx
@@ -7,11 +7,9 @@ import Stats from '../stats';
 
 const UserPage = props => {
     const { data, err, Loader } = useGet(props.api + (props.search || ''));
-    console.log(props.api + (props.search || ''))
     if (Loader) return <Loader className='mx-auto' />
     if (err) return <h3 className='absolute-middle text-center text-accent-2'>{err}</h3>
     const isCurrentUser = props.userId === props.user?._id, user = getProfile(isCurrentUser ? props.user : data);
-    console.log(data)
     return (
         <>
             <div className='d-grid gap-2'>
--- src/live/profile-page/filters.jsx
+++ src/live/profile-page/filters.jsx
@@ -2,7 +2,7 @@ import PredictrFilters from "../../components/predictr-filters";
 
 export default function Filters() {
     return (
-        <div className="card">
+        <div className="card sticky">
             <b className="text-base mb-4 block">Filters</b>
             <PredictrFilters />
         </div>
