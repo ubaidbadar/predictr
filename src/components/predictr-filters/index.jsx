b8177b07f97a2ea6a0e6e6dd2e56f68d16416484
Ubaid Badar <m.ubaidbadar@gmail.com>
2024-11-10 22:33:01 +0500

    Refactor PredictrFilters to use custom hook for state management and update input handling

--- src/components/predictr-filters/index.jsx
+++ src/components/predictr-filters/index.jsx
@@ -1,41 +1,41 @@
-import { DateCalendar } from '@mui/x-date-pickers';
 import Calendar from '../../icons/calendar';
 import Magnifier from '../../icons/magnifier';
 import styles from './styles.module.scss';
-import CalendarPicker from '../../ui/Calendar';
-import dayjs from 'dayjs';
 import getValues from '../../hoc/form/getValues';
+import useFilter from '../filters-modal/useFilter';
+import updateSearchParams from '../../lib/updateSearchParams';
+import RangePicker from '../../ui/range-picker';
 
 export default function PredictrFilters() {
+    const t = useFilter();
     return (
         <>
-            <form className={`${styles.root}`} onSubmit={e => {
-                e.preventDefault();
-                const values = getValues(e.target).values;
-                console.log(values)
-            }}>
+            <form className={`${styles.root}`}
+                onSubmit={e => {
+                    e.preventDefault();
+                    const values = getValues(e.target).values;
+                    updateSearchParams(values);
+                }}
+            >
                 <label>
                     Accuracy (%)
-                    <input type="number" name='accuracy' placeholder='Enter your accuracy' min={1} />
+                    <input type="number" value={t.accuracy} onChange={t.onChange} name='accuracy' placeholder='Enter your accuracy' min={1} />
                 </label>
                 <div>
                     Date Range
-                    <CalendarPicker
-                        component={(open, date) => (
+                    <RangePicker
+                        onChange={t.onRangeChange}
+                        component={tools => (
+                           <>
                             <label className='w-full relative flex-center'>
-                                <input name='start_date' value={date ? date.format('YYYY/MM/DD') : ""} readOnly onClick={open} onFocus={open} placeholder='From' />
+                                <input name='start_date' value={t.start_date} readOnly onClick={tools.open} onFocus={tools.open} placeholder='From' />
                                 <Calendar className="w-5 absolute right-3" />
                             </label>
-                            // {date ? date.toISOString() : 'From'} <Calendar />
-                        )}
-                    />
-                    <CalendarPicker
-                        component={(open, date) => (
                             <label className='w-full relative flex-center'>
-                                <input name='end_date' value={date ? date.format('YYYY/MM/DD') : ""} readOnly onClick={open} onFocus={open} placeholder='To' />
+                                <input name='end_date' value={t.end_date} readOnly onClick={tools.open} onFocus={tools.open} placeholder='To' />
                                 <Calendar className="w-5 absolute right-3" />
                             </label>
-                            // {date ? date.toISOString() : 'From'} <Calendar />
+                           </>
                         )}
                     />
                 </div>
@@ -43,7 +43,7 @@ export default function PredictrFilters() {
                 <label className={styles.tag}>
                     HashTags
                     <Magnifier />
-                    <input name='hashtags' type="text" placeholder='#tags' />
+                    <input name='hashtags' value={t.hashtags} onChange={t.onChange} type="text" placeholder='#tags' />
                 </label>
                 <button className='btn-primary text-sm'>Apply</button>
             </form>
--- src/live/profile-page/filters.jsx
+++ src/live/profile-page/filters.jsx
@@ -3,7 +3,7 @@ import PredictrFilters from "../../components/predictr-filters";
 export default function Filters() {
     return (
         <div className="card">
-            <b className="text-base">Filters</b>
+            <b className="text-base mb-4 block">Filters</b>
             <PredictrFilters />
         </div>
     )
