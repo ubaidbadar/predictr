2230908da2d51f5113b5c13c96764820c625eaeb
Ubaid Badar <m.ubaidbadar@gmail.com>
2024-11-11 19:30:00 +0500

    Refactor ModalBase component to accept additional className prop; update styling in modal-0.scss for rounded corners and adjust Tailwind config for box shadow values.

--- src/ui/modal_base/index.jsx
+++ src/ui/modal_base/index.jsx
@@ -1,13 +1,13 @@
 import Portal from "../../hoc/portal";
 
-export default function ModalBase({ className, close, children, Root = "div", open, id, title, ...props }) {
+export default function ModalBase({ className, close, children, Root = "div", open, id, title, classM, ...props }) {
     return (
         <Portal id="Modals">
             <div
                 className={`Dialog ${className}`}
                 onClick={e => e.target === e.currentTarget && close()}
             >
-                <Root className="Dialog-Main" {...props} other={typeof Root === "string" ? "" : { close }}>{children}</Root>
+                <Root className={`Dialog-Main ${classM}`} {...props} other={typeof Root === "string" ? "" : { close }}>{children}</Root>
             </div>
         </Portal>
     )
