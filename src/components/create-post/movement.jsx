06ea05cc8b9c735032254fd15b74936842f2ca2f
Ubaid Badar <42312378+ubaidbadar@users.noreply.github.com>
2024-11-18 15:31:02 +0500

    Refactor CreatePost and Movement components to enhance input handling, replace Input with TextField for better UX, and update BoxField styles for improved layout and responsiveness

--- src/components/create-post/movement.jsx
+++ src/components/create-post/movement.jsx
@@ -1,7 +1,8 @@
 import BoxField from "../../ui/boxselect";
-import Input from "../../ui/input";
+import TextField from "../../ui/textfield";
 
 export default function Movement(props) {
+    const values = [3, 5, 7, 10]
     return (
         <>
             {props.getHeader('Movement')}
@@ -10,7 +11,28 @@ export default function Movement(props) {
                 <BoxField className="contain-checkbox" checked={props.estimated_direction === 'Up'} onChange={props.onChange} name="estimated_direction" title="Up" value="Up" type="radio" />
                 <BoxField className="contain-checkbox" checked={props.estimated_direction === 'Down'} onChange={props.onChange} name="estimated_direction" value="Down" title="Down" type="radio" />
             </div>
-            <Input type="number" min={1} name="estimated_change_percent" value={props.estimated_change_percent} onChange={props.onChange} label="Percentage (%)" />
+            <div className="flex flex-wrap gap-2">
+                <TextField min={1}
+                    type="number"
+                    className="w-full"
+                    title="Percentage (%)"
+                    onChange={props.onChange}
+                    placeholder="Enter percentage"
+                    name="estimated_change_percent"
+                    value={props.estimated_change_percent}
+                />
+                {values.map(item => (
+                    <BoxField
+                        key={item}
+                        value={item}
+                        title={`${item}%`}
+                        className="flex-1"
+                        onChange={props.onChange}
+                        name="estimated_change_percent"
+                        checked={item == props.estimated_change_percent}
+                    />
+                ))}
+            </div>
             <div className="Modal-Footer">
                 <button onClick={props.back} className="btn-primary" disabled={!props.estimated_change_percent}>Done</button>
             </div>
